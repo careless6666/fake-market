2e6b8880f5ca93eab00da29cbae837dc4bd7b8bb
alex <alusov@ozon.ru>
2022-03-11 23:35:11 +0300

    add cors

--- backend/app.ts
+++ backend/app.ts
@@ -2,19 +2,21 @@ import express from 'express';
 import Router from "./routes";
 import swaggerUI = require('swagger-ui-express')
 import morgan from "morgan";
-
+import cors from 'cors';
+import * as core from "express-serve-static-core";
 
 const app = express();
 const port = 3001;
+
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+app.use(express.json());
+
+AddCors(app);
+
 app.get('/', (req, res) => {
     res.send('The sedulous hyena ate the antelope!');
 });
-
-
-
-app.use(morgan("tiny"));
-
-app.use(express.static("public"));
 
 app.use(
     "/docs",
@@ -31,3 +33,20 @@ app.use(Router);
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
+
+function AddCors(app: core.Express) {
+    const corsOpts = {
+        origin: '*',
+
+        methods: [
+            'GET',
+            'POST',
+        ],
+
+        allowedHeaders: [
+            'Content-Type',
+        ],
+    };
+
+    app.use(cors(corsOpts));
+}
